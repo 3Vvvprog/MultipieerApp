744e3ea0a457c6afc208fbd3a306b9e45a8a7485
3Vvv <333vovk@mail.ru>
2024-12-25 19:57:32 +0300

    Initial Commit

---
+++ MultipieerApp/MultipieerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipieerAppApp.swift
+//  MultipieerApp
+//
+//  Created by Вячеслав Вовк on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultipieerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
